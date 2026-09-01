79f5901e8cc4798bc7d094c28ff1f8180bb2c59f
Zoymusk <zoyamuskaan20@gmail.com>
2026-09-01 19:39:03 +0530

    Time: 1 ms (68.27%), Space: 44.5 MB (24.72%) - LeetHub

---
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countSeniors(String[] details) {
+        int c=0;
+
+        for(String s: details){
+            String age= s.substring(11,13);
+            int a= Integer.parseInt(age);
+            if(a>60){
+                c++;
+            }
+        }
+        return c;
+    }
+}
